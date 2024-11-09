79779e02a11aea5ae3bb8de21e597ea4f89aba69
gallo-json <gallojsantiago@gmail.com>
2024-11-09 13:27:53 -0500

    got my voice as a vector

--- main.py
+++ main.py
@@ -1,39 +1,27 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from langchain_openai import ChatOpenAI
 from os import environ
+import requests
+import json
 
+TEAM_API_KEY = environ.get('TEAM_API_KEY')
+PROXY_ENDPOINT = environ.get('PROXY_ENDPOINT')
+CARTESIA_KEY = environ.get('CARTESIA_KEY')
 
-llm = ChatOpenAI(
-    openai_api_key=environ.get('API_KEY'), 
-    openai_api_base=environ.get('API_BASE'),
-    model="gpt-4o"
-)
+url = "https://api.cartesia.ai/voices/clone/clip"
+headers = {
+    "X-API-Key": CARTESIA_KEY, 
+    "Cartesia-Version": "2024-06-10",
+}
 
-template = """
-We recorded conversations of a bunch of people with their best friends. Here are their transcriptions:
+files = {
+    'clip': ('jose.mp3', open('jose.mp3', 'rb'), 'audio/mpeg'),
+}
+data = {
+    'enhance': 'true'
+}
+response = requests.post(url, headers=headers, files=files, data=data)
 
-John: ....
-
-Hamza: ....
-
-William: ....
-
-James: ....
-
-Here is our transcription:
-{transcription}
-
-Based on our transcription, return the top 3 people that would seem most compatible for rooming. Base on the following guidelines:
-- Tone of voice
-- Personality
-- Interests
-- Liveliness
-"""
-
-prompt = template.format(transcription='blah blah')
-
-response = llm.invoke(prompt)
-
-print(response.content)
+print(response.status_code)
+print(response.json())
