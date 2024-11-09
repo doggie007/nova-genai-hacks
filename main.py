7c831ee142ceec0f0970842dbf49e6e3eaf7644d
gallo-json <gallojsantiago@gmail.com>
2024-11-09 11:32:50 -0500

    figured out API usage type shi

---
+++ main.py
@@ -0,0 +1,16 @@
+from langchain_openai import ChatOpenAI
+
+
+llm = ChatOpenAI(
+    openai_api_key="sk-1-nnzryEbytTnAqTcIyH7Q", 
+    openai_api_base="https://nova-litellm-proxy.onrender.com",
+    model="gpt-4o"
+)
+
+template = """Generate a kids story based on this prompt: {query}"""
+
+prompt = template.format(query='My sons dad was in jail. Write a story to make sure he doesnt end like him')
+
+response = llm.invoke(prompt)
+
+print(response.content)
